de8a04067b54b644d8a7270fb398ef70bf9a061a
Hem Pun <hemthegentlmen@gmail.com>
2023-03-22 10:31:28 +0545

    Time Populate added

--- app.js
+++ app.js
@@ -1,10 +1,48 @@
 const inputContainer = document.querySelector('#input-container'),
       countdownForm = document.querySelector('#countdownForm'),
-      dateEl = document.querySelector('#date-picker');
+      dateEl = document.querySelector('#date-picker'),
+      coutdownEl = document.querySelector('#countdown'),
+      countdownElTitle = document.querySelector('#countdown-title'),
+      countdownBtn = document.querySelector('#countdown-button'),
+      timeElements = document.querySelectorAll('span');
 
 
 let countdownTitle =  '';
 let countdownDate = '';
+let countdownValue = Date;
+
+const second = 1000
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour *24;
+
+
+// Populate Countdown /Complete UI
+const updateDOM =() =>{
+    const now = new Date().getTime();
+    const distance = countdownValue - now;
+
+    const days = Math.floor(distance / day)
+    const hours = Math.floor((distance % day) / hour) //Get remaning decimal point form days and divide by hour
+    const minutes = Math.floor((distance % hour) /minute)
+    const seconds = Math.floor((distance % minute) /second)
+    // console.log(days,hours,minutes,seconds);
+
+    // Populate Countdown
+    countdownElTitle.textContent = `${countdownTitle}`
+    timeElements[0].textContent = `${days}`;
+    timeElements[1].textContent = `${hours}`;
+    timeElements[2].textContent = `${minutes}`;
+    timeElements[3].textContent = `${seconds}`;
+
+    // Hide Inpute Container
+    inputContainer.hidden = true;
+
+    // Show ccountdown
+    coutdownEl.hidden = false;
+
+}
+
 // Set Date Input Min with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min',today)
@@ -14,7 +52,11 @@ const updateCountdown =(e) =>{
     e.preventDefault();
     countdownTitle = e.srcElement[0].value;
     countdownDate = e.srcElement[1].value
-    
+    // Get Number version of current Date and Update DOM
+    countdownValue = new Date(countdownDate).getTime()
+    // console.log(countdownValue);
+    updateDOM();
+
 }
 // Event Listner
 countdownForm.addEventListener('submit',updateCountdown)
