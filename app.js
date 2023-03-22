5661e3aa7fe851b9c00b9865dbe9722de54c2fab
Hem Pun <hemthegentlmen@gmail.com>
2023-03-22 07:42:43 +0545

    Count Down Tittle and Count Down date added

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const inputContainer = document.querySelector('#input-container'),
       dateEl = document.querySelector('#date-picker');
 
 
+let countdownTitle =  '';
+let countdownDate = '';
 // Set Date Input Min with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min',today)
+
+// Takes value from Form Input
+const updateCountdown =(e) =>{
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value
+    
+}
+// Event Listner
+countdownForm.addEventListener('submit',updateCountdown)
